0a8d7388a977c4284f8370c4e32100318e82fcfc
15johare <15johare@stjosephsrush.ie>
2018-01-12 12:43:04 +0000

    Update translator.py

--- translator.py
+++ translator.py
@@ -44,4 +44,62 @@ elif word=="lamp":
 elif word=="flower":
  print("une fleur")
  
+ elif word=="snake":
+  print("un serpent")
+  
+ elif word=="the cinema":
+  print("le cinéma")
+  
+ elif word=="the bus":
+  print("le bus")
+  
+ elif word=="the cafe":
+  print("le café")
+  
+ elif word=="the restaurant":
+  print("le restaurant")
+  
+ elif word=="the agenda":
+  print("l'agenda")
+  
+ elif word=="the book":
+  print("le livre")
+  
+ elif word=="the teacher":
+  print("le prof")
+  
+ elif word=="secondary school":
+  print("le collège")
+  
+ elif word=="football":
+  print("le foot")
+  
+ elif word=="the harmonica":
+  print("l'harmonica")
+  
+ elif word=="the stuff":
+  print("les trucs")
+  
+ elif word=="the animal":
+  print("l'animal")
+  
+ elif word=="school":
+  print("l'école")
+  
+ elif word=="goodbye":
+  print("au revoir")
+  
+ elif word=="I'm fine thank you":
+  print("ça va bien merci")
+  
+ elif word=="so so":
+  print("comme ci, comme ça")
+  
+ elif word=="and you?":
+  print("et toi?")
+  
+ elif word==""what is your name":
+  print("comment tu t'appelles")
+  
+ 
  
